30ef56699e40a79606e5bdd9e698ad800c0cd560
donkikochan <donkikochan@gmail.com>
2014-03-06 17:05:46 +0100

    no message

---
+++ aventura/EditorCell.h
@@ -0,0 +1,13 @@
+//
+//  EditorCell.h
+//  aventura
+//
+//  Created by DonKikochan on 06/03/14.
+//  Copyright (c) 2014 owlab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EditorCell : UITableViewCell
+
+@end
---
+++ aventura/EditorCell.m
@@ -0,0 +1,29 @@
+//
+//  EditorCell.m
+//  aventura
+//
+//  Created by DonKikochan on 06/03/14.
+//  Copyright (c) 2014 owlab. All rights reserved.
+//
+
+#import "EditorCell.h"
+
+@implementation EditorCell
+
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
+{
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
